8fe477e9ae56270a12f8b53f49197db687c1140c
Admin <openjaroslav@gmail.com>
2020-07-14 16:10:06 +1000

    Django Rest Complete example done.

---
+++ api/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from books.models import Book
+from .serializers import BookSerializer
+
+
+class BookAPIView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
--- library_project/settings.py
+++ library_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'books.apps.BooksConfig',
+    'api.apps.ApiConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
